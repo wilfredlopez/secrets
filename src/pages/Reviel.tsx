aed5ed21e8fc62729e4a29d60dd541bbf7656f94
Wilfred Lopez <cubamc@gmail.com>
2020-05-25 00:16:36 -0400

    ready for prod

--- src/pages/Reviel.tsx
+++ src/pages/Reviel.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import { Secrets } from "components"
 import { Title } from "components/styled"
-import { Link } from "react-router-dom"
 
 interface Props {}
 
@@ -10,9 +9,6 @@ const Reviel = (props: Props) => {
     <div className="text-center">
       <Title>Reviel The Secret</Title>
       <Secrets />
-      <div>
-        <Link to="/code?code">TEST LINK</Link>
-      </div>
     </div>
   )
 }
